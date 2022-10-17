70ab7cf0356b90f4a3493f935ccc003096c5d1ab
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 11:41:31 +0200

    todoList: delete tasks from the list

--- todolistTest.py
+++ todolistTest.py
@@ -3,6 +3,16 @@ todo = input("Ajouter une tâche ou taper 'ok' s'il n'y a rien à ajouter");
 while todo!="ok":
     todoList.append(todo)
     todo = input("Ajouter une tâche ou taper 'ok' s'il n'y a rien à ajouter");
-if todo=="ok":
+if todo == "ok":
     print("Voici votre liste de tâches:")
     print(todoList)
+
+doneIndex=input("Pour marquer une tâche comme finie, tapez 0.")
+while todoList!=[]:
+   del todoList[int(doneIndex)]
+   print(todoList)
+   if todoList!=[]:
+     doneIndex = input("Pour marquer une tâche comme finie, tapez 0.")
+if todoList==[]:
+    print("Félicitations, vous avez accompli toutes vos tâches.")
+
