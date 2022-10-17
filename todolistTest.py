9b30704f9cf48fb35f422d2ebc573b65900a3ad0
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 12:04:01 +0200

    todoList: check todoList's length before deleting item

--- todolistTest.py
+++ todolistTest.py
@@ -1,4 +1,5 @@
 todoList=[]
+# 1 - Ajouter des tâches à la liste
 todo = input("Ajouter une tâche ou taper 'ok' s'il n'y a rien à ajouter");
 while todo!="ok":
     todoList.append(todo)
@@ -7,12 +8,21 @@ if todo == "ok":
     print("Voici votre liste de tâches:")
     print(todoList)
 
-doneIndex=input("Pour marquer une tâche comme finie, tapez 0.")
+# 2 - Supprimer des tâches de la liste
+done=input("Pour marquer une tâche comme finie, tapez son numero.")
+doneIndex = int(done)
 while todoList!=[]:
-   del todoList[int(doneIndex)]
-   print(todoList)
-   if todoList!=[]:
-     doneIndex = input("Pour marquer une tâche comme finie, tapez 0.")
+    if doneIndex < len(todoList):
+        del todoList[doneIndex]
+        print(todoList)
+        if todoList!=[]:
+            done = input("Pour marquer une tâche comme finie, tapez son numero.")
+            doneIndex = int(done)
+    else:
+        print("Vous n'avez pas autant de tâches dans votre liste")
+        if todoList != []:
+            done = input("Pour marquer une tâche comme finie, tapez son numero.")
+            doneIndex = int(done)
 if todoList==[]:
     print("Félicitations, vous avez accompli toutes vos tâches.")
 
