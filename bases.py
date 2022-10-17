94f8efe85c4f0b6d93d88bd48893ea419b1674ac
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 10:38:14 +0200

    lists

--- bases.py
+++ bases.py
@@ -7,3 +7,13 @@ for i in [0, 1, 2]:
 print("Fin")
 # ( équivalent de [0,1,2].foreach(i=> print("valeur :", i)) )
 
+# 2 - Les listes:
+
+# lister des nombres dans un intervalle (range) donné:
+liste = list(range(8,16));
+print(liste)
+
+# lister des nombres, à partir de 1, jusqu'à 10, de 2 en 2
+listImpaire = list(range(1,10,2))
+print(listImpaire)
+
