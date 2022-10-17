9f197c498f1e19be3d48580d375bc3cf92ea0471
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-10-17 10:28:05 +0200

    test on array

---
+++ bases.py
@@ -0,0 +1,9 @@
+#On écrit les commentaires comme ça
+
+# 1 - Répéter une opération sur toutes les veleurs d'un tableau:
+# "pour chaque élément i de [0, 1, 2], affiche ("valeur": i)"
+for i in [0, 1, 2]:
+    print("valeur :", i)
+print("Fin")
+# ( équivalent de [0,1,2].foreach(i=> print("valeur :", i)) )
+
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 username = input("Bonjour, quel est votre nom?");
-print("Hello " + username);
+print("Hello", username);
